2533320c518381803f1923bab53437525b9aa24a
3rd-Eden <info@3rd-Eden.com>
2013-05-16 21:30:40 +0200

    [minor] Start on the relaiable message queue

---
+++ lua/send.lua
@@ -0,0 +1,42 @@
+--
+-- Some default values that get automatically replaced by our script
+-- pre-processor in the Leverage library. This allows us maintain control over
+-- the output using JavaScript, which is __a w e s o m e__.
+--
+
+local namespace = {leverage::namespace}
+local backlog = {leverage::backlog}
+local expire = {leverage::expire}
+
+--
+-- The arguments that are recieved from the script.
+--
+local channel = assert(KEYS[1], 'The channel key is missing')
+local message = assert(ARGV[1], 'The message argument is missing')
+
+--
+-- We increase the id to get a unique message id for this message.
+--
+local id = redis.call('incr', namespace .. '::msg-id', 1)
+
+--
+-- Our id exceeded the backlog, reset it, this way we also override our "old"
+-- data and keep the database clean.
+--
+if id > backlog then
+  id = redis.call('set', namespace ..'::msg-id', 0)
+end
+
+--
+-- Now that we've gathered all data we can do some magic persistance / relaiable
+-- messaging. Add a backup of the data so it can be retrieved if we miss
+-- a message. We are going to publish the message to the channel and include the
+-- id of our message.
+--
+redis.call('setex', namespace .. '::backlog::' .. id, expire, message)
+redis.call('publish', namespace .. '::' .. channel, cjson.encode({
+  message = message,
+  id      = id
+}))
+
+return id
