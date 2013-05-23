6ff12b9de9b8e050e9d77663d716053297aabe0c
3rd-Eden <info@3rd-Eden.com>
2013-05-23 16:08:03 +0200

    [test] Added test for ordered reliable pub/sub

--- test/common.js
+++ test/common.js
@@ -43,7 +43,50 @@ function leverage(pubsub, options) {
   return new Leverage(redis(), options);
 }
 
-exports.expect = expect;
+/**
+ * Kills Redis connections for the last issued command.
+ *
+ * @param {String} cmd The command, lowercase
+ * @param {Function} cb The callback to issue the kill
+ */
+function kill(cmd, cb) {
+  var client = redis();
+
+  return client.client('list', function list(err, data) {
+    if (err) {
+      client.quit();
+      return cb(err);
+    }
+
+    //
+    // Parse the client response because the `redis` client can't parse it.
+    //
+    data = data.split('\n').filter(Boolean).map(function linefeed(data) {
+      return data.split(' ').reduce(function parser(set, line) {
+        var kv = line.split('=');
+        set[kv[0]] = kv[1].trim();
+
+        return set;
+      }, {});
+    }).filter(function figureout(data) {
+      return data.cmd === cmd;
+    });
+
+    if (data[0]) return client.client('kill', data[0].addr, function idontcare() {
+      client.quit();
+      cb.apply(this, arguments);
+    });
+
+    client.quit();
+    cb(new Error('No connections matching the given command'));
+  });
+}
+
+//
+// Expose the methods.
+//
+exports.kill = kill;
 exports.redis = redis;
+exports.expect = expect;
 exports.leverage = leverage;
 exports.Leverage = Leverage;
---
+++ test/pubsub.test.js
@@ -0,0 +1,56 @@
+describe('Leverage (reliable pubsub)', function () {
+  'use strict';
+
+  var common   = require('./common')
+    , Leverage = common.Leverage
+    , leverage = common.leverage
+    , expect   = common.expect
+    , redis    = common.redis
+    , kill     = common.kill;
+
+  describe('ordered', function () {
+    it('maintains order when the subscription is dropped', function (done) {
+      this.timeout(10000);
+
+      var pubsub = leverage(true)
+        , timeout
+        , id = 0;
+
+      pubsub.subscribe('<channelname>', {
+        ordered: true,
+        replay: 0
+      }).on('<channelname>::message', function (msg, msgid) {
+        expect(msg).to.be.a('string');
+        expect(msgid).to.be.a('number');
+        expect(msg).to.equal('omg pubsub');
+
+        clearTimeout(timeout);
+
+        if (id && id + 1 !== msgid) {
+          throw new Error('The message are out of order');
+        }
+
+        timeout = setTimeout(function () {
+          throw new Error('Pub/Sub response was not received in a timely manner');
+        }, 500);
+      });
+
+      var publish = setInterval(function publish() {
+        pubsub.publish('<channelname>', 'omg pubsub');
+      }, 50);
+
+      setTimeout(function murder() {
+        kill('subscribe', function murdered(err) {
+          if (err) return done(err);
+
+          setTimeout(function alive() {
+            clearInterval(publish);
+            pubsub.destroy();
+
+            done();
+          }, 2000);
+        });
+      }, 2000);
+    });
+  });
+});
